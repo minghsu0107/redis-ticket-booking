4748d06ccdb0ccc261a5e05e57f79a5786b001e1
Hao-Ming, Hsu <50090692+minghsu0107@users.noreply.github.com>
2021-07-02 22:55:20 +0800

    fix typo

--- model.go
+++ model.go
@@ -7,5 +7,5 @@ type Ticket struct {
 	ID        int        `gorm:"primaryKey"` // auto-increment
 	UserID    int        // bigint
 	CreatedAt int64      `gorm:"autoUpdateTime:milli"`
-	SoldAt    *time.Time // use pointer to avoid zero-value fieldx
+	SoldAt    *time.Time // use pointer to avoid zero-value field
 }
